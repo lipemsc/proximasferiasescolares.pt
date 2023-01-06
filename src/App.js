6064a409ff4267c944f1c2caa203205a14241656
Filipe <lipe.msc@gmail.com>
2023-01-06 00:07:17 +0000

    Add background photo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends React.Component {
   render = () => {
     return (
       <div className='background'>
-        <MiddleScreenCounter />
+        <div className='backgroundFilter'>
+          <MiddleScreenCounter />
+        </div>
       </div>
     );
   }
