7bd56a01ba4bcddf442d2c9b62cd1b6d6a21540c
Filipe <lipe.msc@gmail.com>
2023-02-10 17:13:57 +0000

    Partially finished the fetching process.
    Used axios library for a easier process.
    Need to find a way to hide the countdown or show a loading screen while the
    request has not been made.

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from 'axios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+var date = ""
 
 const fetchApi = () => {
+  axios.get("https://api.proximasferiasescolares.pt/").then((response) => {
+    date = response.data
+  })
+  /* 
   fetch("https://api.proximasferiasescolares.pt/").then((response) => {
     console.log(response.body);
-  })
+  }) */
 }
 
 const renderPage = () => {
   root.render(
     <div>
-      <App dateString = "2023-02-28T18:10:00.000"/>
+      <App dateString = {date}/>
     </div>
     
   );
