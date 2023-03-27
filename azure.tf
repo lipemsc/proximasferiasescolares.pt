07c2fb5d23fdfcdd58e482d881e8d7187bdd02af
Filipe <lipe.msc@gmail.com>
2023-03-27 11:11:52 +0100

    Add terraform files to deploy to azure.
    Make sure you have are logged in using azure cli

---
+++ azure.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "ferias" {
+  name     = var.resource_group_name
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "webstorageaccount" {
+  name                     = "feriaswebpage"
+  resource_group_name      = azurerm_resource_group.ferias.name
+  location                 = azurerm_resource_group.ferias.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  static_website {
+    index_document = "index.html"
+  }
+}
+
+resource "azurerm_storage_container" "webstoragecontainer" {
+  name                  = "test"
+  storage_account_name  = azurerm_storage_account.webstorageaccount.name
+  container_access_type = "container"
+}
+
+resource "azurerm_storage_blob" "blobs" {
+  for_each = fileset("${path.root}/${var.build_folder}", "**")
+  name = each.key
+  storage_account_name   = azurerm_storage_account.webstorageaccount.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  source                 = "${path.root}/build/${each.key}"
+  content_type           = ""
+}
---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+    description = "Resource group name to be created in your Azure account"
+    type = string
+}
+
+variable "build_folder" {
+    description = "Folder where the build files are created"
+    type = string
+}
